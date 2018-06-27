90765cd23d508d8bdd0b7464a4e754ecf435e17f
Dan Nguyen <dansonguyen@gmail.com>
2018-06-27 16:24:17 -0700

    sqlfixes

--- {{ cookiecutter.repo_name }}/src/dba/vanilla/bootstrap.sql
+++ {{ cookiecutter.repo_name }}/src/dba/vanilla/bootstrap.sql
@@ -3,16 +3,16 @@
 .echo on
 
 -- drop/create new tables
-.open ./data/processed/DATABASE_NAME.sqlite
+.open ./data/processed/__DATABASE_NAME__.vanilla.sqlite
 .read ./src/dba/vanilla/schema.sql
 
 -- import the data
 .changes on
-.import ./data/raw/DATAFILE_NAME.csv TABLE_NAME
+.import ./data/raw/__DATAFILE_NAME__.csv __TABLE_NAME__
 
 -- index the data
 .read ./src/dba/vanilla/indexes.sql
 
 
 -- remove the repeated headers
-DELETE FROM TABLE_NAME WHERE SOME_ID_COL = 'SOME_ID_COL';
+DELETE FROM __TABLE_NAME__ WHERE __SOME_ID_COL__ = '__SOME_ID_COL__';
